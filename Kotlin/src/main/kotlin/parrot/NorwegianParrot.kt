61e4949869c9319692ac02e1ae3a1ab76f436bec
vdufau <vincent.dufau@4sh.fr>
2024-09-30 15:58:36 +0200

    c super

--- Kotlin/src/main/kotlin/parrot/NorwegianParrot.kt
+++ Kotlin/src/main/kotlin/parrot/NorwegianParrot.kt
@@ -7,7 +7,9 @@ class NorwegianParrot(numberOfCoconuts: Int, voltage: Double, isNailed: Boolean)
     voltage,
     isNailed
 ) {
-    override fun getSpeed(): Double = if (isNailed) 0.0 else min(24.0, voltage * BASE_SPEED)
+    override fun getSpeed(): Double = min(24.0, voltage * BASE_SPEED)
+        .takeUnless { isNailed }
+        ?: 0.0
 
     override fun getCry(): String = if (voltage > 0) "Bzzzzzz" else "..."
 }
